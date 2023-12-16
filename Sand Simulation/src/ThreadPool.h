ecc475b474eedec65b28933c2d268f4d47442237
Kade <97352407+OO0oo0oOOooo@users.noreply.github.com>
2023-12-15 23:41:35 -0700

    Threadpool

--- Sand Simulation/src/ThreadPool.h
+++ Sand Simulation/src/ThreadPool.h
@@ -8,6 +8,24 @@
 #include <functional>
 
 class ThreadPool {
+public:
+    explicit ThreadPool(size_t thread_count = std::thread::hardware_concurrency())
+    {
+        if (!thread_count)
+            return;
+    }
+
+    ~ThreadPool()
+    {
+
+    }
+
+    ThreadPool(const ThreadPool&) = delete;
+    ThreadPool(const ThreadPool&&) = delete;
+    ThreadPool& operator = (const ThreadPool&) = delete;
+    ThreadPool& operator = (ThreadPool&&) = delete;
+
+private:
     std::vector<std::thread> workers;
     std::queue<std::function<void()>> tasks;
     std::mutex queue_mutex;
