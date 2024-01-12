377e45ea4322fc67e4089065a213cc07ef7b5bf3
Kade <97352407+OO0oo0oOOooo@users.noreply.github.com>
2024-01-12 02:26:31 -0700

    Water

--- Sand Simulation/src/Chunk.cpp
+++ Sand Simulation/src/Chunk.cpp
@@ -127,7 +127,6 @@ void Chunk::UpdateActive()
 				for (int j = 0; j <= 4; j++)
 				{
 
-
 					glm::ivec2 neighbourPosition = cellPosition + NeighbourTable[j];
 					Cell neighbour = world->GetChunkFromWorldPos(neighbourPosition)->GetCell(neighbourPosition, WorldSpace);
 
